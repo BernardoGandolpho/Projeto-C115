0244c602d7f5d6ffe7287b1be5e5a1723c22430b
avnerjose <avnerj.g.r@gmail.com>
2022-09-06 14:52:46 -0300

    fix: solving CORS and python cache issues

--- app/chatbot.py
+++ app/chatbot.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI, HTTPException, Body, Path, Query, Depends
 from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
 
 from .answers import answers
 from .models import StudentModel
 from .repository.student_repository import StudentRepository
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/chatbot")
 def start_conversation():
@@ -37,7 +47,7 @@ def conversation(
             )
 
         response = answers[1]
-        
+        response["possible_answers"] = []
         for i in student["subjects"]:
             response["possible_answers"].append(i["subject_id"])
 
@@ -45,48 +55,51 @@ def conversation(
         student = repository.list_one_student(id)
 
         response = answers[2]
-        
+        response["possible_answers"] = []
         for i in student["subjects"][materia]["grade"].keys():
             response["possible_answers"].append(i)
 
     elif step == 3:
         student = repository.list_one_student(id)
-
         grade = student["subjects"][materia]["grade"]
-
         response = answers[3]
-
         str_grade = list(grade.keys())[nota]
 
-        response["messages"].append(f"O aluno {student['name']} teve a {str_grade} igual a {grade[str_grade]}")
+        messages = []
+        messages.append(response["messages"][0])
+        messages.append(
+            f"O aluno {student['name']} teve a {str_grade} igual a {grade[str_grade]}")
+        response["messages"] = messages 
 
     else:
         return "O Front retornou algum treco esquisito"
 
-    
     return response
 
 
-@app.get("/database")
+@ app.get("/database")
 def list_students(
-        repository: StudentRepository = Depends(StudentRepository)
-    ):
-    
+    repository: StudentRepository = Depends(StudentRepository)
+
+
+):
+
     students = repository.list_all()
 
     if students is not None:
         return {"Estudantes": students}
-    
-    raise HTTPException(status_code=404, detail=f"Não existem estudantes matriculados")
+
+    raise HTTPException(
+        status_code=404, detail=f"Não existem estudantes matriculados")
 
 
-@app.post("/database", response_model=StudentModel)
+@ app.post("/database", response_model=StudentModel)
 def create_student(
         student: StudentModel = Body(...),
         repository: StudentRepository = Depends(StudentRepository)
 ):
     new_student = jsonable_encoder(student)
-    
+
     response = repository.add(new_student)
 
     if type(response) is type(HTTPException(status_code=400)):
@@ -95,7 +108,7 @@ def create_student(
     return response
 
 
-@app.delete("/database/{id}")
+@ app.delete("/database/{id}")
 def delete_student(
     id: int = Path(...),
     repository: StudentRepository = Depends(StudentRepository)
